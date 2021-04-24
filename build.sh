2e17874d3d62f680d3eff83acb847127e84b1b5d
Ballantyne, Bob <ballantyne.robert@gmail.com>
2021-04-24 18:08:54 -0400

    fix stdout issues due to bad redirection for capture

--- build.sh
+++ build.sh
@@ -8,12 +8,14 @@
 
 # declarations of MUST HAVE globals
 PROGRAM_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}")"
+PROGRAM_NAME="$(basename "${BASH_SOURCE[0]}" | sed 's|.sh$||')"
 LOGFILE="$(pwd)/${PROGRAM_NAME}.log"
 
-declare -r LIB_NAME='build'
+
+declare -r LIB_NAME="$PROGRAM_NAME"
 declare -A OPTS=( [cmd]='build.all' )
 declare -a ARGS=()
+
 
 # load our libraries
 declare -r loader="${PROGRAM_DIR}/bashlib/appenv.bashlib"
@@ -23,18 +25,15 @@ if [ ! -e "$loader" ]; then
 fi
 source "$loader"
 appenv.initialize "$@"
+appenv.capture "$LOGFILE"  # capture stdin+stdout to logfile
 build.checkSetup
 
 # our main script
 declare -i status=0
-{
-
-    if [ "${#ARGS[*]}" -gt 0 ]; then
-        "${OPTS['cmd']}" ${OPTS['oper']:-} "${ARGS[@]:-}" && status=$? || status=$?
-    else
-        "${OPTS['cmd']}" ${OPTS['oper']:-} && status=$? || status=$?
-    fi
-    [ -d "$logDir" ] && [ $(ls -1A "$logDir" | wc -l) -gt 0 ] || rmdir "$logDir"
-    exit $status 
-
-} 2>&1 | tee "$LOGFILE"
+if [ "${#ARGS[*]}" -gt 0 ]; then
+    "${OPTS['cmd']}" ${OPTS['oper']:-} "${ARGS[@]:-}" && status=$? || status=$?
+else
+    "${OPTS['cmd']}" ${OPTS['oper']:-} && status=$? || status=$?
+fi
+[ -d "$logDir" ] && [ $(ls -1A "$logDir" | wc -l) -gt 0 ] || rmdir "$logDir"
+exit $status 
